bc1b6b40b3e3da72819d9c185540fa845b197e6d
Elchin Mamedaliev <elchin321@gmail.com>
2021-07-22 17:43:19 -0400

    updated UI added routs

--- budget_app_1.0/client/budget_app/src/History.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import './Top.css';
-import ListTransactions from './ListTransactions.js';
-
-function History() {
-  return <ListTransactions />;
-}
-
-export default History;
